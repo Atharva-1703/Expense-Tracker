edda2008fcc2a15b80a3e2a22f445a7a77ec13a1
Atharva-1703 <141930344+Atharva-1703@users.noreply.github.com>
2024-11-24 11:33:41 +0530

    Configure react redux and query

---
+++ frontend/src/redux/slice/authSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+// ? initial state
+const authSlice = createSlice({
+  name: "auth",
+  initialState: {
+    user: null,
+  },
+  // ? reducers
+  reducers: {
+    loginAction: (state, action) => {
+      state.user = action.payload;
+    },
+    // ? logout action
+    logoutAction: (state, action) => {
+      state.user = null;
+    },
+  },
+});
+
+// ! actions
+export const { loginAction, logoutAction } = authSlice.actions;
+// ? reducer
+const authReducer = authSlice.reducer;
+export default authReducer;
